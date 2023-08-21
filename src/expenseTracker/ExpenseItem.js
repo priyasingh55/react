0927e976bcbdbcd5a96053faa212ec192317c6f3
priyasingh55 <138660538+priyasingh55@users.noreply.github.com>
2023-08-21 14:30:26 +0530

    2 files modified

--- src/expenseTracker/ExpenseItem.js
+++ src/expenseTracker/ExpenseItem.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import './ExpenseItem.css';
 
-
-export default function ExpenseItem(props){
- 
-  return(
-    
-
+export default function ExpenseItem() {
+  return (
     <div className="expense-item">
       <div>August 21st 2023</div>
-      <div className='expense-item_description'> 
-      <h2>{ props.title } </h2>
-      <div className='expense-item_price'>100$</div>
+      <div className="expense-item_description">
+        <h2>Room Rent </h2>
+        <div className="expense-item_price">100$</div>
       </div>
-
     </div>
-  )
+  );
 }
--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ root.render(
   <StrictMode>
     <App />
     <Subcomponent />
+    
  
 
 
